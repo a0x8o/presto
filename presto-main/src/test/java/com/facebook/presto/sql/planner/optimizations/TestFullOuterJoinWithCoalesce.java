f98b6a3ddd6e24993fbf96d3c8ef0559823341fa
Alex Barreto <axbarreto@lavabit.com>
2019-09-10 17:35:54 -0700

    Close OrcDataSource in OrcPageFileRewriter

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestFullOuterJoinWithCoalesce.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestFullOuterJoinWithCoalesce.java
@@ -28,7 +28,7 @@ import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.projec
 import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.values;
 import static com.facebook.presto.sql.planner.plan.AggregationNode.Step.PARTIAL;
 import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.LOCAL;
-import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.REMOTE;
+import static com.facebook.presto.sql.planner.plan.ExchangeNode.Scope.REMOTE_STREAMING;
 import static com.facebook.presto.sql.planner.plan.ExchangeNode.Type.GATHER;
 import static com.facebook.presto.sql.planner.plan.ExchangeNode.Type.REPARTITION;
 import static com.facebook.presto.sql.planner.plan.JoinNode.Type.FULL;
@@ -40,13 +40,13 @@ public class TestFullOuterJoinWithCoalesce
     public void testFullOuterJoinWithCoalesce()
     {
         assertDistributedPlan("SELECT coalesce(r.a, ts.a) " +
-                "FROM (" +
-                "   SELECT coalesce(t.a, s.a) AS a " +
-                "   FROM (VALUES (1), (2), (3)) t(a) " +
-                "   FULL OUTER JOIN (VALUES (1), (4)) s(a)" +
-                "   ON t.a = s.a) ts " +
-                "FULL OUTER JOIN (VALUES (2), (5)) r(a) " +
-                "ON ts.a = r.a",
+                        "FROM (" +
+                        "   SELECT coalesce(t.a, s.a) AS a " +
+                        "   FROM (VALUES (1), (2), (3)) t(a) " +
+                        "   FULL OUTER JOIN (VALUES (1), (4)) s(a)" +
+                        "   ON t.a = s.a) ts " +
+                        "FULL OUTER JOIN (VALUES (2), (5)) r(a) " +
+                        "ON ts.a = r.a",
                 anyTree(
                         project(
                                 ImmutableMap.of("expr", expression("coalesce(r, ts)")),
@@ -59,9 +59,65 @@ public class TestFullOuterJoinWithCoalesce
                                                         join(
                                                                 FULL,
                                                                 ImmutableList.of(equiJoinClause("t", "s")),
-                                                                exchange(REMOTE, REPARTITION, anyTree(values(ImmutableList.of("t")))),
+                                                                exchange(REMOTE_STREAMING, REPARTITION, anyTree(values(ImmutableList.of("t")))),
                                                                 exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("s"))))))),
                                         exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("r"))))))));
+    }
+
+    @Test
+    public void testDuplicateJoinClause()
+    {
+        assertDistributedPlan("SELECT coalesce(r.a, ts.a) " +
+                        "FROM (" +
+                        "   SELECT coalesce(t.a, s.a) AS a " +
+                        "   FROM (VALUES (1), (2), (3)) t(a) " +
+                        "   FULL OUTER JOIN (VALUES (1), (4)) s(a)" +
+                        "   ON t.a = s.a AND t.a = s.a) ts " +
+                        "FULL OUTER JOIN (VALUES (2), (5)) r(a) " +
+                        "ON ts.a = r.a",
+                anyTree(
+                        project(
+                                ImmutableMap.of("expr", expression("coalesce(r, ts)")),
+                                join(
+                                        FULL,
+                                        ImmutableList.of(equiJoinClause("ts", "r")),
+                                        project(
+                                                project(
+                                                        ImmutableMap.of("ts", expression("coalesce(t, s)")),
+                                                        join(
+                                                                FULL,
+                                                                ImmutableList.of(equiJoinClause("t", "s"), equiJoinClause("t", "s")),
+                                                                exchange(REMOTE_STREAMING, REPARTITION, anyTree(values(ImmutableList.of("t")))),
+                                                                exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("s"))))))),
+                                        exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("r"))))))));
+    }
+
+    @Test
+    public void testDuplicatePartitionColumn()
+    {
+        assertDistributedPlan("SELECT coalesce(r.a, ts.a), coalesce(ts.b, r.b) " +
+                        "FROM (" +
+                        "   SELECT coalesce(t.a, s.a) AS a, coalesce(t.a, s.b) AS b" +
+                        "   FROM (VALUES (1), (2), (3)) t(a) " +
+                        "   FULL OUTER JOIN (VALUES (1, 1), (4, 4)) s(a, b)" +
+                        "   ON t.a = s.a AND t.a = s.b) ts " +
+                        "FULL OUTER JOIN (VALUES (2, 2), (5, 5)) r(a, b) " +
+                        "ON ts.a = r.a and ts.b = r.b",
+                anyTree(
+                        project(
+                                ImmutableMap.of("tsra", expression("coalesce(ra, tsa)"), "tsrb", expression("coalesce(tsb, rb)")),
+                                join(
+                                        FULL,
+                                        ImmutableList.of(equiJoinClause("tsa", "ra"), equiJoinClause("tsb", "rb")),
+                                        project(
+                                                project(
+                                                        ImmutableMap.of("tsa", expression("coalesce(ta, sa)"), "tsb", expression("coalesce(ta, sb)")),
+                                                        join(
+                                                                FULL,
+                                                                ImmutableList.of(equiJoinClause("ta", "sa"), equiJoinClause("ta", "sb")),
+                                                                exchange(REMOTE_STREAMING, REPARTITION, anyTree(values(ImmutableList.of("ta")))),
+                                                                exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("sa", "sb"))))))),
+                                        exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("ra", "rb"))))))));
     }
 
     @Test
@@ -75,7 +131,7 @@ public class TestFullOuterJoinWithCoalesce
                         "ON t.a = r.a " +
                         "GROUP BY 1",
                 anyTree(exchange(
-                        REMOTE,
+                        REMOTE_STREAMING,
                         REPARTITION,
                         aggregation(
                                 ImmutableMap.of(),
@@ -90,7 +146,7 @@ public class TestFullOuterJoinWithCoalesce
                                                                 join(
                                                                         FULL,
                                                                         ImmutableList.of(equiJoinClause("t", "s")),
-                                                                        exchange(REMOTE, REPARTITION, anyTree(values(ImmutableList.of("t")))),
+                                                                        exchange(REMOTE_STREAMING, REPARTITION, anyTree(values(ImmutableList.of("t")))),
                                                                         exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("s")))))),
                                                         exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("r")))))))))));
     }
@@ -106,7 +162,7 @@ public class TestFullOuterJoinWithCoalesce
                         "ON t.a = r.a " +
                         "GROUP BY 1",
                 anyTree(exchange(
-                        REMOTE,
+                        REMOTE_STREAMING,
                         REPARTITION,
                         aggregation(
                                 ImmutableMap.of(),
@@ -121,7 +177,7 @@ public class TestFullOuterJoinWithCoalesce
                                                                 join(
                                                                         FULL,
                                                                         ImmutableList.of(equiJoinClause("t", "s")),
-                                                                        exchange(REMOTE, REPARTITION, anyTree(values(ImmutableList.of("t")))),
+                                                                        exchange(REMOTE_STREAMING, REPARTITION, anyTree(values(ImmutableList.of("t")))),
                                                                         exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("s")))))),
                                                         exchange(LOCAL, GATHER, anyTree(values(ImmutableList.of("r")))))))))));
     }
